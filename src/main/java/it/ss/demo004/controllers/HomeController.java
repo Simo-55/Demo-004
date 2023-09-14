71a9c01658ffa331ebba981d91d06e1c22ca5ce7
Simo-55 <simonesella99@gmail.com>
2023-09-14 23:01:30 +0200

    test#001

---
+++ src/main/java/it/ss/demo004/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package it.ss.demo004.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home()  {
+        String index = "index";
+        return index;
+    }
+
+}
